ac1297344358f9dbc541cd85572228bdaab4bfbd
ChoiTaesan <mijosan@naver.com>
2020-11-22 23:32:33 +0900

    study : spring boot & aws & ci/cd

---
+++ src/main/java/com/toyproject/springbootwebservice/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.toyproject.springbootwebservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
---
+++ src/test/java/com/toyproject/springbootwebservice/controller/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.toyproject.springbootwebservice.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class HelloControllerTest {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("hello가 리턴 된다.")
+    @Test
+    public void helloReturn() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+}
