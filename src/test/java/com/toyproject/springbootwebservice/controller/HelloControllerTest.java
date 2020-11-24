265d1a81feac9dc63f9bc902e9434d2cad769f96
ChoiTaesan <mijosan@naver.com>
2020-11-24 23:50:32 +0900

    study : 76page

--- src/test/java/com/toyproject/springbootwebservice/controller/HelloControllerTest.java
+++ src/test/java/com/toyproject/springbootwebservice/controller/HelloControllerTest.java
@@ -7,8 +7,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
     
     @Autowired
@@ -22,6 +26,22 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+                
+
     }
 
 }
